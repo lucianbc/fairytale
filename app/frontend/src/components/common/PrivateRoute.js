9c39162ee4e185a99f2389a35dacacd1f20d1727
Lucian Boaca <lucianboaca97@gmail.com>
2019-04-06 16:34:46 +0300

    Removed .idea related files and added them to gitignore. Fixed header title and links collapse under the medium breakpoint

--- app/frontend/src/components/common/PrivateRoute.js
+++ app/frontend/src/components/common/PrivateRoute.js
@@ -4,22 +4,22 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 const PrivateRoute = ({ component: Component, auth, ...rest }) => (
-    <Route
-        {...rest}
-        render={props => {
-            if (auth.isLoading) {
-                return <h2>Loading...</h2>;
-            } else if (!auth.isAuthenticated) {
-                return <Redirect to="/login" />;
-            } else {
-                return <Component {...props} />;
-            }
-        }}
-    />
+  <Route
+    {...rest}
+    render={props => {
+      if (auth.isLoading) {
+        return <h2>Loading...</h2>;
+      } else if (!auth.isAuthenticated) {
+        return <Redirect to="/login" />;
+      } else {
+        return <Component {...props} />;
+      }
+    }}
+  />
 );
 
 const mapStateToProps = state => ({
-    auth: state.auth
+  auth: state.auth
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
--- app/frontend/src/components/layouts/Header.js
+++ app/frontend/src/components/layouts/Header.js
@@ -5,76 +5,76 @@ import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
 
 export class Header extends Component {
-    static propTypes = {
-        auth: PropTypes.object.isRequired,
-        logout: PropTypes.func.isRequired
-    };
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired
+  };
 
-    render() {
-        const { isAuthenticated, user } = this.props.auth;
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-        const authLinks = (
-            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                <span className="navbar-text mr-3">
-                    <strong>{user ? `Welcome ${user.username}` : ""}</strong>
-                </span>
-                <li className="nav-item">
-                    <button
-                        onClick={this.props.logout}
-                        className="nav-link btn btn-info btn-sm text-light"
-                    >
-                        Logout
+    const authLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <span className="navbar-text mr-3">
+          <strong>{user ? `Welcome ${user.username}` : ""}</strong>
+        </span>
+        <li className="nav-item">
+          <button
+            onClick={this.props.logout}
+            className="nav-link btn btn-info btn-sm text-light"
+          >
+            Logout
           </button>
-                </li>
-            </ul>
-        );
+        </li>
+      </ul>
+    );
 
-        const guestLinks = (
-            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                <li className="nav-item">
-                    <Link to="/register" className="nav-link">
-                        Register
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
           </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">
-                        Login
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
           </Link>
-                </li>
-            </ul>
-        );
+        </li>
+      </ul>
+    );
 
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <div className="container">
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarTogglerDemo01"
-                        aria-controls="navbarTogglerDemo01"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">
-                            Fairytale Gone Bad
-            </a>
-                    </div>
-                    {isAuthenticated ? authLinks : guestLinks}
-                </div>
-            </nav>
-        );
-    }
+    return (
+      <nav className="navbar navbar-expand-md navbar-light bg-light mb-2">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Fairytale Gone Bad
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
+  auth: state.auth
 });
 
 export default connect(
-    mapStateToProps,
-    { logout }
+  mapStateToProps,
+  { logout }
 )(Header);
